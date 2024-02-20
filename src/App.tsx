15f5cb9ac6fb7b33444b2c7b77bdbddc0bdc78a5
Yan Silitskiy <kucerenko171@gmail.com>
2024-02-20 15:48:25 +0200

    feat: helper function classNames

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import {Suspense} from "react";
 import {Link, Route, Routes} from "react-router-dom";
 import './styles/index.scss';
 import {useTheme} from "./ theme/useTheme";
+import {classNames} from "./helpers/classNames/classNames";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Change theme</button>
             <Link to={'/'}>Main page</Link>
             <Link to={'/about'}>About page</Link>
